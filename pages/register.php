8210890090075eba2136e77aeb30dbc4f8ba1bfe
RaimondsCipkins <56106606+RaimondsCipkins@users.noreply.github.com>
2020-11-25 23:12:11 +0200

    3.0

--- pages/register.php
+++ pages/register.php
@@ -62,39 +62,91 @@
         <div class="content">
             <div id="gallery">
                 <figure>
-                    <form action="login" method="post">
-                        <form action="/action_page.php">
-                            <div class="form-group">
-                                <label for="name">Vārds:</label>
-                                <input type="text" class="form-control" id="name">
-                            </div>
-                            <div class="form-group">
-                                <label for="name">Uzvārds:</label>
-                                <input type="text" class="form-control" id="name">
-                            </div>
-                            <div class="form-group">
-                                <label for="name">Email:</label>
-                                <input type="email" class="form-control" id="email">
-                                <div class="form-group">
-                                    <label for="name">Telefons:</label>
-                                    <input type="email" class="form-control" id="email">
-                            <div class="checkbox">
-                                <label><input type="checkbox"> Remember me</label>
-                            </div>
-                            <button type="submit" class="btn btn-default">Submit</button>
-                        </form>
-                    </div>
-                    <hr>
-                    <div class="container signin">
-                        <h2> Add a comment </h2>
-                        <textarea rows="4" cols="50" name="comment" form="usrform" maxlength="200">
-                Enter text here...</textarea>
-                        <input type="submit" value="Submit">
-                        <!-- ################################################################################################ -->
-                    </div>
-                    <!-- ################################################################################################ -->
-                    <!-- / main body -->
-                    <div class="clear"></div>
+                    <?php
+                    // define variables and set to empty values
+                    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+                    $name = $email = $gender = $comment = $website = "";
+
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        if (empty($_POST["name"])) {
+                            $nameErr = "Name is required";
+                        } else {
+                            $name = test_input($_POST["name"]);
+                            // check if name only contains letters and whitespace
+                            if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+                                $nameErr = "Only letters and white space allowed";
+                            }
+                        }
+
+                        if (empty($_POST["email"])) {
+                            $emailErr = "Email is required";
+                        } else {
+                            $email = test_input($_POST["email"]);
+                            // check if e-mail address is well-formed
+                            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                                $emailErr = "Invalid email format";
+                            }
+                        }
+
+                        if (empty($_POST["website"])) {
+                            $website = "";
+                        } else {
+                            $website = test_input($_POST["website"]);
+                            // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+                            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+                                $websiteErr = "Invalid URL";
+                            }
+                        }
+
+                        if (empty($_POST["comment"])) {
+                            $comment = "";
+                        } else {
+                            $comment = test_input($_POST["comment"]);
+                        }
+
+                        if (empty($_POST["gender"])) {
+                            $genderErr = "Gender is required";
+                        } else {
+                            $gender = test_input($_POST["gender"]);
+                        }
+                    }
+
+                    function test_input($data) {
+                        $data = trim($data);
+                        $data = stripslashes($data);
+                        $data = htmlspecialchars($data);
+                        return $data;
+                    }
+                    ?>
+
+                    <h2>Validācija</h2>
+                    <p><span class="error">* required field</span></p>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        Name: <input type="text" name="name" value="<?php echo $name;?>">
+                        <span class="error">* <?php echo $nameErr;?></span>
+                        <br><br>
+                        E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+                        <span class="error">* <?php echo $emailErr;?></span>
+                        <br><br>
+                        Website: <input type="text" name="website" value="<?php echo $website;?>">
+                        <span class="error"><?php echo $websiteErr;?></span>
+                        <br><br>
+                        <input type="submit" name="submit" value="Submit">
+                    </form>
+
+                    <?php
+                    echo "<h2>Your Input:</h2>";
+                    echo $name;
+                    echo "<br>";
+                    echo $email;
+                    echo "<br>";
+                    echo $website;
+                    echo "<br>";
+                    echo $comment;
+                    echo "<br>";
+                    echo $gender;
+                    ?>
+
     </main>
 </div>
 <!-- ################################################################################################ -->
