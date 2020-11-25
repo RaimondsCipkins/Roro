1329b330391b272176bd7448e3849705f3831949
RaimondsCipkins <56106606+RaimondsCipkins@users.noreply.github.com>
2020-11-25 21:52:49 +0200

    Pievienots .htacces un lapa ar kļūdu

---
+++ kludas/not_found.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>EverTap Official</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        .navbar {
+            margin-bottom: 0;
+            border-radius: 0;
+        }
+
+        .row.content {height: 450px}
+
+        .sidenav {
+            padding-top: 20px;
+            background-color: #f1f1f1;
+            height: 100%;
+        }
+
+        footer {
+            background-color: #555;
+            color: white;
+            padding: 15px;
+        }
+
+        @media screen and (max-width: 767px) {
+            .sidenav {
+                height: auto;
+                padding: 15px;
+            }
+            .row.content {height:auto;}
+        }
+    </style>
+</head>
+<body>
+        </div>
+        <div class="col-sm-8 text-left">
+            <font color="#000000">
+              <h1>Sorry, picu trūkums</h1>
+            </font>
+
+        </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
